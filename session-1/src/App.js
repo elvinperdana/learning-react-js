fed269e1c65cb3e27a92ec33dbe8f4866cb7a212
elvin-phire <elvinperdana.en@gmail.com>
2023-04-10 14:55:45 +0700

    first commit

---
+++ session-1/src/App.js
@@ -0,0 +1,15 @@
+
+import Introduction from "./component/introduction";
+import Profile from "./component/profile";
+
+function App() {
+  return (
+    <>
+        <Profile></Profile>
+        <Introduction name="Elvin"></Introduction>
+        <Introduction name="Priska"></Introduction>
+    </>
+  );
+}
+
+export default App;
---
+++ session-1/src/component/introduction.js
@@ -0,0 +1,10 @@
+function Introduction(props) {
+    return (
+        <div>
+            <h1>Introduction</h1>
+            <p>My name is <span>{props.name}</span></p>
+        </div>
+    );
+}
+
+export default Introduction;
