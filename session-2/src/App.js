6a6c45986aa2096644c1b82d5cf8676af5d0bafc
elvin-phire <elvinperdana.en@gmail.com>
2023-04-11 09:41:01 +0700

    finish

--- session-2/src/App.js
+++ session-2/src/App.js
@@ -1,15 +1,18 @@
 import React, {useEffect, useState} from "react";
-import Introduction from "./component/introduction";
-import Profile from "./component/profile";
+// import Introduction from "./component/introduction";
+// import Profile from "./component/profile";
 
 function App() {
     // variable getData is a getter, variable setData is a setter
     const [getData, setData] = useState(1)
-    const [getDataLambo, setDataLambo] = useState(1)
+    const [getDataLambo, setDataLambo] = useState(0)
 
     useEffect(() => {
-        if (getData%5 == 0){
-            setDataLambo((data) => data + 1)
+        if (getData % 5 === 0) {
+            setDataLambo(getData / 5)
+        }
+        if(getData < 5){
+            setDataLambo(0)
         }
     }, [getData]) // '[]' is make the function running just 1 time
 
@@ -18,8 +21,8 @@ function App() {
             {/*lamborghini will increase every 5 ferrari*/}
             <h5>I Have {getDataLambo} Lamborghini</h5>
             <h1>I Have {getData} Ferrari</h1>
-            <button onClick={() => setData((prev) => prev + 1 )}>Add Ferrari</button>
-            {getData > 1 ? <button onClick={() => setData((prev) => prev - 1 )}>Decrease Ferrari</button> : ''}
+            <button onClick={() => setData((prev) => prev + 1)}>Add Ferrari</button>
+            {getData > 1 ? <button onClick={() => setData((prev) => prev - 1)}>Decrease Ferrari</button> : ''}
         </>
     );
 }
